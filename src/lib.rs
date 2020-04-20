1c17c1ae51109ff6d6b201c68cf149ad333c398b
charles <dinneyacharles007@gmail.com>
2020-04-20 12:32:13 +0100

    add test

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,31 @@
 // this function makes the conversion.
 // if the argument ends with C, it assumes its a temperature in celsius.
 // if argument ends with F, it assumes the its a temperature in fahrenheit.
-pub fn convert(value: &mut String) {
+pub fn convert(value: &mut String) -> f32 {
+    let mut result = 0.0;
     if value.trim().ends_with("C") {
-        println!("calculating for fahrenheit...");
-        let fahrenheit = (trunc(value) / 9.0) * 32.0;
-        println!("{}F", fahrenheit);
+        println!("converting to fahrenheit...");
+        result = trunc(value) * 1.8 + 32.0;
+        println!("{:.2}F", result);
     } else if value.trim().ends_with("F") {
-        println!("calculating for celsius...");
-        let celsius = (trunc(value) * 9.0) / 32.0;
-        println!("{}C", celsius);
+        println!("converting to celsius...");
+        result = trunc(value) - 32.0 / 1.8;
+        println!("{:.2}C", result);
     } else {
         println!("Invalid value, value must end with either 'C' or 'F'.");
     }
+
+    result
 }
 
 // This function takes an argument, makes a new string by removing the last character.
 // function also converts string value to number and returns the number.
-fn trunc(arg: &mut String) -> f64 {
+fn trunc(arg: &mut String) -> f32 {
     let new_value = match arg.truncate(arg.len() - 2) {
         () => arg,
     };
 
-    let calc_value: f64 = match new_value.trim().parse() {
+    let calc_value: f32 = match new_value.trim().parse() {
         Ok(num) => num,
         Err(e) => {
             println!("{}", e);
@@ -31,4 +34,14 @@ fn trunc(arg: &mut String) -> f64 {
     };
 
     return calc_value;
+}
+
+#[test]
+fn celsius_fahrenheit() {
+    assert_eq!(convert(&mut "44C".to_string()), 39.2);
+}
+
+#[test]
+fn fahrenheit_celsius() {
+    assert_eq!(convert(&mut "44F".to_string()), -13.777779);
 }
